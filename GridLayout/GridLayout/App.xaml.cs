1e20a545a6acae2c0e690704cfbc1c1ea6a81eb4
vidapogosoft <vidapogosoft@hotmail.com>
2021-03-19 08:07:16 -0500

    Talleres dia 2
    Ejercicios practicos con diseño de pantallas en XAML

---
+++ GridLayout/GridLayout/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using GridLayout.Views;
+
+namespace GridLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+
+            MainPage = new Home();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
